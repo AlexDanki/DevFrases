a3c6917eb45151eff23ec9a8fffedebff4f64b6a
Alexsandro Duarte <pocmusic321@gmail.com>
2025-08-14 20:06:01 -0300

    criando botões de categorias de forma dinâmica

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,31 @@ import './App.css'
 
 function App() {
 
+  const [frase, setFrase] = useState("");
+
+  const categorias = [
+    {
+      id: 0,
+      categoria: "Bom dia",
+      frase: [
+        "Que seu dia seja cheio de alegrias e realizações",
+        "Que a felicidade te acompanhe em cada momento",
+        "Desejo a você um dia produtivo e cheio de boas energias",
+        "Que a esperança renove suas forças e te inspire a seguir em frente",
+      ]
+    },
+    {
+      id: 1,
+      categoria: "Esperança",
+      frases: [
+        "A esperança é o sonho do homem acordado",
+        "Devemos aceitar a decepção finita, mas nunca perder a esperança infinita",
+        "Uma montanha de memórias nunca é igual a uma pequena esperança",
+        "Quando você chegar ao fim de sua corda, dê um nó e segure-se" 
+      ]
+    }
+  ]
+
   return (
     <div className="container">
       <img className="logo" src={logoImg} alt="logo image" />
@@ -12,15 +37,18 @@ function App() {
       <h2 className="title">Gerador</h2>
 
       <section className='buttons-categoria'>
-        <button className='btn_categoria'>Bom dia</button>
-        <button className='btn_categoria'>Boa noite</button>
-        <button className='btn_categoria'>Esperança</button>
-        <button className='btn_categoria'>Engraçada</button>
+        {
+          categorias.map((item, index)=>(
+
+            <button className='btn_categoria'>{item.categoria}</button>
+
+          ))
+        }
       </section>
 
       <button className="btn_gerar">Gerar</button>
 
-      <p className="frase-gerada">"Esse aqui é um exeplo de texto gerado pela a aplicação"</p>
+      { frase && (<p className="frase-gerada">{`"${frase}"`}</p>) }
 
     </div>
   )
